b87709ac813cc20da200871019ec7e247a338c40
alu10467008 <alundu008@ieselcaminas.org>
2022-04-27 18:16:51 +0200

    filereader

--- src/FileContactProvider.java
+++ src/FileContactProvider.java
@@ -1,16 +1,26 @@
 
 import java.io.*;
+import java.util.LinkedList;
 import java.util.List;
 
 
 public class FileContactProvider implements IContactsProvider{
     @Override
-    public List<Contact> loadContacts() throws FileNotFoundException {
-
+    public List<Contact> loadContacts() throws IOException {
+        List<Contact> contactslist = new LinkedList<>();
         FileReader fileReader = new FileReader("/home/INFORMATICA/alu10467008/Escriptori/agenda/resources/contacts.txt");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String[] contacts;
+        do {
+            try {
+                contacts = bufferedReader.readLine().split(";");
+                contactslist.add(new Contact(Integer.parseInt(contacts[0]), contacts[1], contacts[2], contacts[3], contacts[4]));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (bufferedReader.readLine() != null );
 
-        bufferedReader.readLine()
+
         return null;
     }
 
--- src/IContactsProvider.java
+++ src/IContactsProvider.java
@@ -1,8 +1,9 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public interface IContactsProvider {
-    List<Contact> loadContacts() throws FileNotFoundException;
+    List<Contact> loadContacts() throws IOException;
     void add(Contact contact);
     void remove(Contact contact);
     void update(Contact contact);
